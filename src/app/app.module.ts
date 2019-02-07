1f038bfe1f4fa9bf9ef247c0494d901b42c3d863
BastienRA <bastien.ranc@iut-rodez.fr>
2019-02-07 10:05:43 +0100

    develop the sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SuiModule} from 'ng2-semantic-ui';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
