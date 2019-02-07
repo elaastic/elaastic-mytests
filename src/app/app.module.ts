90293e272cbc5c1618877bf92131042e823ea1d6
Gromnur <flavienregis@gmail.com>
2019-02-07 10:48:52 +0100

    develop list-test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SuiModule} from 'ng2-semantic-ui';
+import { ListTestComponent } from './list-test/list-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListTestComponent
   ],
   imports: [
     BrowserModule,
